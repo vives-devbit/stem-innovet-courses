d5eef0fc6a76748ef437d64d0a1ebc356261b6e7
Nico De Witte <nico.dewitte@outlook.com>
2020-01-13 20:34:11 +0100

    Refactor navbar a bit

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-app-bar
+        dark
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-toolbar-title>IoT, slimme school 2.0 </v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn
+          color="red"
+          class="ma-2 white--text"
+        >
+          Courses
+          <v-icon right dark>mdi-notebook-multiple</v-icon>
+        </v-btn>
+  
+      </v-app-bar>
 
     <v-content>
+      <Course />
       <Course />
     </v-content>
     <Footer/>
