6f6e87c99c4d051c490f3ba42a00b57613dcf9c7
Nico De Witte <nico.dewitte@outlook.com>
2020-01-29 19:34:05 +0100

    Change titles of features

--- src/components/CorePoints.vue
+++ src/components/CorePoints.vue
@@ -23,7 +23,7 @@
         xl="4"
       >
         <v-card outlined>
-          <v-card-title class="justify-center primary--text"><h2>Samenwerking</h2></v-card-title>
+          <v-card-title class="justify-center primary--text"><h2>Open Source</h2></v-card-title>
           <v-card-text class="text--primary text-center">
             <div>Something about git and stuff. Github Rules and all</div>
             <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum id felis a fringilla. Etiam pretium, eros mattis luctus sollicitudin, leo sapien varius ex, vitae suscipit ligula mauris vitae augue. Nullam nibh tellus, molestie sed sagittis in, porttitor et lacus. Etiam venenatis dolor non tincidunt cursus. Suspendisse eu fermentum elit. Fusce convallis nec nunc laoreet elementum. Vivamus metus massa, consequat a gravida id, venenatis a nibh.</div>
@@ -38,7 +38,7 @@
         xl="4"
       >
         <v-card outlined>
-          <v-card-title class="justify-center primary--text"><h2>Samenwerking</h2></v-card-title>
+          <v-card-title class="justify-center primary--text"><h2>Online Beschikbaar</h2></v-card-title>
           <v-card-text class="text--primary text-center">
             <div>Something about git and stuff. Github Rules and all</div>
             <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum id felis a fringilla. Etiam pretium, eros mattis luctus sollicitudin, leo sapien varius ex, vitae suscipit ligula mauris vitae augue. Nullam nibh tellus, molestie sed sagittis in, porttitor et lacus. Etiam venenatis dolor non tincidunt cursus. Suspendisse eu fermentum elit. Fusce convallis nec nunc laoreet elementum. Vivamus metus massa, consequat a gravida id, venenatis a nibh.</div>
