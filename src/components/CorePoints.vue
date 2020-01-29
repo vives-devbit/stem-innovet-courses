0c9f01ac26b276ab221c6a3fba1ef40f0b2e3aac
Nico De Witte <nico.dewitte@outlook.com>
2020-01-29 19:13:01 +0100

    First attempt at features

--- src/components/CorePoints.vue
+++ src/components/CorePoints.vue
@@ -1,7 +1,12 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col sm12 md6 lg3>
+  <v-container class="secondary lighten-3" fluid>
+    <v-row class="mx-12">
+      <v-col cols="12"
+        sm="12"
+        md="4"
+        lg="4"
+        xl="4"
+      >
         <v-card outlined>
           <v-card-title class="justify-center red--text"><h2>Samenwerking</h2></v-card-title>
           <v-card-text class="text--primary text-center">
@@ -10,6 +15,37 @@
           </v-card-text>
         </v-card>
       </v-col>
+
+      <v-col cols="12"
+        sm="12"
+        md="4"
+        lg="4"
+        xl="4"
+      >
+        <v-card outlined>
+          <v-card-title class="justify-center red--text"><h2>Samenwerking</h2></v-card-title>
+          <v-card-text class="text--primary text-center">
+            <div>Something about git and stuff. Github Rules and all</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum id felis a fringilla. Etiam pretium, eros mattis luctus sollicitudin, leo sapien varius ex, vitae suscipit ligula mauris vitae augue. Nullam nibh tellus, molestie sed sagittis in, porttitor et lacus. Etiam venenatis dolor non tincidunt cursus. Suspendisse eu fermentum elit. Fusce convallis nec nunc laoreet elementum. Vivamus metus massa, consequat a gravida id, venenatis a nibh.</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12"
+        sm="12"
+        md="4"
+        lg="4"
+        xl="4"
+      >
+        <v-card outlined>
+          <v-card-title class="justify-center red--text"><h2>Samenwerking</h2></v-card-title>
+          <v-card-text class="text--primary text-center">
+            <div>Something about git and stuff. Github Rules and all</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum id felis a fringilla. Etiam pretium, eros mattis luctus sollicitudin, leo sapien varius ex, vitae suscipit ligula mauris vitae augue. Nullam nibh tellus, molestie sed sagittis in, porttitor et lacus. Etiam venenatis dolor non tincidunt cursus. Suspendisse eu fermentum elit. Fusce convallis nec nunc laoreet elementum. Vivamus metus massa, consequat a gravida id, venenatis a nibh.</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
     </v-row>
   </v-container>
 </template>
