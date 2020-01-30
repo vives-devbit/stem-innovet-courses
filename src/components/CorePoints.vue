982fca25c3bda39fc4b40f6924708cff0867a877
Nico De Witte <nico.dewitte@outlook.com>
2020-01-30 19:15:40 +0100

    Add feature with audience

--- src/components/CorePoints.vue
+++ src/components/CorePoints.vue
@@ -23,10 +23,17 @@
         xl="3"
       >
         <v-card outlined>
-          <v-card-title class="justify-center primary--text"><h2>Open Source</h2></v-card-title>
-          <v-card-text class="text--primary text-center">
-            <div>Something about git and stuff. Github Rules and all</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum id felis a fringilla. Etiam pretium, eros mattis luctus sollicitudin, leo sapien varius ex, vitae suscipit ligula mauris vitae augue. Nullam nibh tellus, molestie sed sagittis in, porttitor et lacus. Etiam venenatis dolor non tincidunt cursus. Suspendisse eu fermentum elit. Fusce convallis nec nunc laoreet elementum. Vivamus metus massa, consequat a gravida id, venenatis a nibh.</div>
+          <v-card-title class="justify-center primary--text"><h2>Doelpubliek</h2></v-card-title>
+          <v-card-text class="text--primary">
+            <p>
+              De cursussen zijn enerzijds op maat gemaakt voor leerlingen van de <strong>3de graad secundair onderwijs</strong> uit de richtingen: elektriciteit-elektronica, industriële wetenschappen en industriële-ICT.
+            </p>
+            <p>
+              Anderzijds komen <strong>richtingen uit het STEM domein</strong> hiervoor ook in aanmerking: technologische wetenschappen en engineering, nformatie- en communicatiewetenschappen, mechatronica en industriële-ICT.
+            </p>
+            <p>
+              Natuurlijk is het cursusmateriaal eveneens geschikt voor <strong>iedereen die interesse heeft in het domein van Internet of Things (IoT)</strong>.
+            </p>
           </v-card-text>
         </v-card>
       </v-col>
