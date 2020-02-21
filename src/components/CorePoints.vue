d2f8dc80bb71769a802176ed1c01bad0cbf797bd
Nico De Witte <nico.dewitte@outlook.com>
2020-02-21 16:55:39 +0100

    Add highlight on cooperation - closes #8

--- src/components/CorePoints.vue
+++ src/components/CorePoints.vue
@@ -10,8 +10,7 @@
         <v-card outlined>
           <v-card-title class="justify-center primary--text"><h2>Samenwerking</h2></v-card-title>
           <v-card-text class="text--primary text-center">
-            <div>Something about git and stuff. Github Rules and all</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum id felis a fringilla. Etiam pretium, eros mattis luctus sollicitudin, leo sapien varius ex, vitae suscipit ligula mauris vitae augue. Nullam nibh tellus, molestie sed sagittis in, porttitor et lacus. Etiam venenatis dolor non tincidunt cursus. Suspendisse eu fermentum elit. Fusce convallis nec nunc laoreet elementum. Vivamus metus massa, consequat a gravida id, venenatis a nibh.</div>
+            <div>Het ontwikkelen van cursusmateriaal en oefeningen vergt veel tijd. Samenwerken door materiaal te delen via open source kan hierbij helpen. Voor het bewaren van het cursusmateriaal wordt gebruik gemaakt van <a href="https://github.com/" target="_blank">GitHub</a>. De weergave van de cursus gebeurt via <a href="https://www.netlify.com" target="_blank">Netlify</a>. Een cursus rond GitHub en Netlify is te vinden op <a href="https://devbit-git-course.netlify.com/" target="_blank">https://devbit-git-course.netlify.com</a>. Ook uw bijdrage wordt gewaardeerd! Stuur gerust een <a href="mailto:kris.werbrouck@sint-rembert.be" target="_blank">e-mail</a> wanneer u ook aan dit project wil meewerken door een bijdrage te leveren en een eigen cursus op het platform wil plaatsen.</div>
           </v-card-text>
         </v-card>
       </v-col>
