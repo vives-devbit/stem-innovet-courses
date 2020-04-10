f875ea1e207980e7c90b8fe96ab32179dc7146d7
Nico De Witte <nico.dewitte@outlook.com>
2020-04-10 12:27:27 +0200

    Make corepoints flat v-cards

--- src/components/CorePoints.vue
+++ src/components/CorePoints.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
-    <v-row justify="center" align="center">
+    <v-row justify="center" align="top">
       <v-col cols="12"
         sm="12"
         md="4"
         lg="4"
         xl="3"
       >
-        <v-card outlined>
+        <v-card flat>
           <v-card-title class="justify-center primary--text"><h2>Samenwerking</h2></v-card-title>
           <v-card-text class="text--primary text-center">
             <div>Het ontwikkelen van cursusmateriaal en oefeningen vergt veel tijd. Samenwerken door materiaal te delen via open source kan hierbij helpen. Voor het bewaren van het cursusmateriaal wordt gebruik gemaakt van <a href="https://github.com/" target="_blank">GitHub</a>. De weergave van de cursus gebeurt via <a href="https://www.netlify.com" target="_blank">Netlify</a>. Een cursus rond GitHub en Netlify is te vinden op <a href="https://devbit-git-course.netlify.com/" target="_blank">https://devbit-git-course.netlify.com</a>. Ook uw bijdrage wordt gewaardeerd! Stuur gerust een <a href="mailto:kris.werbrouck@sint-rembert.be" target="_blank">e-mail</a> wanneer u ook aan dit project wil meewerken door een bijdrage te leveren en een eigen cursus op het platform wil plaatsen.</div>
@@ -21,7 +21,7 @@
         lg="4"
         xl="3"
       >
-        <v-card outlined>
+        <v-card flat>
           <v-card-title class="justify-center primary--text"><h2>Doelpubliek</h2></v-card-title>
           <v-card-text class="text--primary">
             <p>
@@ -37,7 +37,7 @@
         lg="4"
         xl="3"
       >
-        <v-card outlined>
+        <v-card flat>
           <v-card-title class="justify-center primary--text"><h2>Omschrijving</h2></v-card-title>
           <v-card-text class="text--primary text-center">
             <div>Alle onderdelen van een IoT-omgeving komen voor in het cursusaanbod: van het opmeten van sensordata met Arduino tot het visueel weergeven op een cloudplatform of eigen server met Raspberry PI.</div>
