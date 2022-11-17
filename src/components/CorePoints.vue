76840954b548cbc3c5f78ba233db417c8e62978e
Nico De Witte <nico.dewitte@outlook.com>
2022-11-17 08:46:57 +0100

    Fix CorePoints colors

--- src/components/CorePoints.vue
+++ src/components/CorePoints.vue
@@ -8,7 +8,7 @@
         xl="3"
       >
         <v-card flat>
-          <v-card-title class="text-h5 font-weight-bold text-primary text-center">Samenwerking</v-card-title>
+          <v-card-title class="text-h5 font-weight-bold text-secondary text-center">Samenwerking</v-card-title>
           <v-card-text class="text-center">
             Het ontwikkelen van cursusmateriaal en oefeningen vergt veel tijd. Samenwerken door materiaal te delen via open source kan hierbij helpen. Voor het bewaren van het cursusmateriaal wordt gebruik gemaakt van <a class="text-primary" href="https://github.com/" target="_blank">GitHub</a>. De weergave van de cursus gebeurt via <a class="text-primary" href="https://www.netlify.com" target="_blank">Netlify</a>. Een cursus rond GitHub en Netlify is te vinden op <a class="text-primary" href="https://devbit-git-course.netlify.com/" target="_blank">https://devbit-git-course.netlify.com</a>. Ook uw bijdrage wordt gewaardeerd! Stuur gerust een <a class="text-primary" href="mailto:kris.werbrouck@sint-rembert.be" target="_blank">e-mail</a> wanneer u ook aan dit project wil meewerken door een bijdrage te leveren en een eigen cursus op het platform wil plaatsen.
           </v-card-text>
@@ -22,7 +22,7 @@
         xl="3"
       >
         <v-card flat>
-          <v-card-title class="text-h5 font-weight-bold text-primary text-center">Doelpubliek</v-card-title>
+          <v-card-title class="text-h5 font-weight-bold text-secondary text-center">Doelpubliek</v-card-title>
           <v-card-text class="text-center">
             Het cursusmateriaal werd ontwikkeld voor leerlingen van de 3de graad secundair onderwijs uit de richtingen Elektriciteit-Elektronica, Industriële Wetenschappen en Industriële-ICT. Het onderwijslandschap is momenteel in verandering en hierop willen we inspelen. Volgende toekomstige richtingen uit het STEM-domein komen in aanmerking om van dit cursusmateriaal gebruik te maken: Informatie- en Communicatiewetenschappen, Technologische Wetenschappen en Engineering, Mechatronica en Industriële-ICT. Het materiaal is ook geschikt voor iedereen die interesse heeft voor ICT, IoT, computernetwerken ...
           </v-card-text>
@@ -36,7 +36,7 @@
         xl="3"
       >
         <v-card flat>
-          <v-card-title class="text-h5 font-weight-bold text-primary text-center">Omschrijving</v-card-title>
+          <v-card-title class="text-h5 font-weight-bold text-secondary text-center">Omschrijving</v-card-title>
           <v-card-text class="text-center">
             Alle onderdelen van een IoT-omgeving komen voor in het cursusaanbod: van het opmeten van sensordata met Arduino tot het visueel weergeven op een cloudplatform of eigen server met Raspberry PI. Er wordt hierbij maximaal gebruik gemaakt van educatief en open source materiaal en software.
           </v-card-text>
